80c165778ffb689851b807650598b53e4a8bb981
Maged Ibrahim <magedibra@yahoo.com>
2024-08-19 16:48:23 +0300

    Add title"

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,12 @@
 import RegisterForm from "@/components/auth/register-form";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
 
 function RegisterPage() {
-  return <RegisterForm />
+  return <RegisterForm />;
 }
 
 export default RegisterPage;
