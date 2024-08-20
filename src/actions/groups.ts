354b4b6537288b64677434a42fe1e6cf55faef32
Maged Ibrahim <magedibra@yahoo.com>
2024-08-20 03:18:17 +0300

    Created Groups and their creation in the database

---
+++ src/actions/groups.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { createGroupSchema } from "@/zod/groups";
+import { getServerAuthSession } from "@/server/auth";
+import { db } from "@/server/db";
+import { type Group } from "@prisma/client";
+
+export async function createGroup(values: unknown) {
+  const session = await getServerAuthSession();
+  if (!session?.user) {
+    return { error: "Not authenticated" };
+  }
+
+  const validatedFields = createGroupSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid input" };
+  }
+
+  const { name, description, isPrivate } = validatedFields.data;
+
+  const group = await db.group.create({
+    data: {
+      name,
+      description,
+      isPrivate,
+      admins: {
+        connect: [{ id: session.user.id }],
+      },
+      participants: {
+        create: [
+          {
+            user: {
+              connect: { id: session.user.id },
+            },
+          },
+        ],
+      },
+    },
+    include: {
+      participants: true,
+    },
+  });
+
+  return { success: true, group };
+}
---
+++ src/zod/groups.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const createGroupSchema = z.object({
+  name: z
+    .string()
+    .min(1, "Name must be 1 character at least")
+    .max(200, "Name must be 200 characters or less"),
+  description: z
+    .string()
+    .max(1024, "Name must be 1024 characters or less")
+    .optional(),
+  isPrivate: z.boolean(),
+});
+
+export type CreateGroupInput = z.infer<typeof createGroupSchema>;
